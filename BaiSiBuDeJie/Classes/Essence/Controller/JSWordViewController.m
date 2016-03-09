407afbab5ac778453e2029311cd1b192a7fe4fd0
leo <leo493852107@163.com>
2016-03-09 22:00:27 +0800

    加载文字帖子数据 and end day 3

--- BaiSiBuDeJie/Classes/Essence/Controller/JSWordViewController.m
+++ BaiSiBuDeJie/Classes/Essence/Controller/JSWordViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "JSWordViewController.h"
+#import "AFNetworking.h"
+#import "UIImageView+WebCache.h"
 
 @interface JSWordViewController ()
+
+/**
+ *  帖子数据
+ */
+@property (nonatomic, strong) NSArray *topics;
 
 @end
 
@@ -17,11 +24,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    // 参数
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"a"] = @"list";
+    params[@"c"] = @"data";
+    params[@"type"] = @"29";
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // 发送请求
+    [[AFHTTPSessionManager manager] GET:@"http://api.budejie.com/api/api_open.php" parameters:params success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
+        self.topics = responseObject[@"list"];
+        
+        [self.tableView reloadData];
+        
+        [responseObject writeToFile:@"/Users/leo/Desktop/duanzi.plist" atomically:YES];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,7 +50,7 @@
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 50;
+    return self.topics.count;
 }
 
 
@@ -40,10 +59,13 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
     
     if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
     }
     
-    cell.textLabel.text = [NSString stringWithFormat:@"%@----%zd", [self class], indexPath.row];
+    NSDictionary *topic = self.topics[indexPath.row];
+    cell.textLabel.text = topic[@"name"];
+    cell.detailTextLabel.text = topic[@"text"];
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:topic[@"profile_image"]] placeholderImage:[UIImage imageNamed:@"defaultUserIcon"]];
     
     return cell;
 }
